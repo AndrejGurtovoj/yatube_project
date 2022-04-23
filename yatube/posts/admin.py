78041db714023b45b0ebe2d1cff58592cf93bf30
Andrej <102756424+AndrejGurtovoj@users.noreply.github.com>
2022-04-24 00:00:43 +0300

    Сообщения

---
+++ yatube/posts/admin.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+from django.contrib import admin
+
+# Register your models here.
+=======
+from django.contrib import admin
+
+# Register your models here.
+>>>>>>> af5cd46fca9c4bd269cee04329f570b663158452
