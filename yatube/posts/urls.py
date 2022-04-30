b16b0bb405857bda227d7060c91d8a8707edb3a3
Андрей Гуртовой <andrej_gurtovoj@mail.ru>
2022-04-30 23:16:49 +0300

    рабочий процесс

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-# posts/urls.py
-=======
->>>>>>> 98b8eadd3bfa24a547381b81a4bf0b81313271c0
 from django.urls import path
+
 from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', views.index),
-<<<<<<< HEAD
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('posts/', views.posts_list),
+    path('posts/<int:pk>/', views.posts_detail),
 ]
-=======
-    path('group/<slug:slug>/', views.group_posts, name='group_list')
-]
->>>>>>> 98b8eadd3bfa24a547381b81a4bf0b81313271c0
