ef3b1850f0a707052b9a8791319022787a363b6d
Андрей Гуртовой <andrej_gurtovoj@mail.ru>
2022-05-12 00:49:37 +0300

    Добавление static

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 
 def index(request):
     templates = 'posts/index.html'
-    return render(request, templates)
+    title = 'Это главная страница проекта Yatube'
+    context = {'title': title,}
+    return render(request, templates, context)
 
 
 def group_posts(request):
-    return HttpResponse('Посты и статьи')
+    templates = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {'title': title,}
+    return render(request, templates, context)
