ce2d7f863d2ad6a8e27f220aa57a7f90e4e7d528
Андрей Гуртовой <andrej_gurtovoj@mail.ru>
2022-05-18 19:42:05 +0300

    Добавления постов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
     templates = 'posts/index.html'
+    Posts = Post.objects.order_by('-pub_date')[:10]
     title = 'Это главная страница проекта Yatube'
-    context = {'title': title,}
+    context = {
+        'Posts': Posts,
+        'title': title
+    }
     return render(request, templates, context)
 
 
-def group_posts(request):
+def group_posts(request, slug):
     templates = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
-    context = {'title': title,}
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {str.group}'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'Posts': posts,
+        'title': title
+    }
     return render(request, templates, context)
